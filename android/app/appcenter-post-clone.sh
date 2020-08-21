ae68aa0bb2dc23a22af23d1e55e29f72a84194b4
Christian Gröschel <christian.groeschel@gmail.com>
2020-08-21 11:39:20 +0200

    adding env file setup

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -22,6 +22,25 @@ flutter doctor -v --android-licenses
 
 echo "Installed flutter to `pwd`/flutter"
 
+
+NODE_VERSION="12.16.0"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+cd ./lib/js_service_kusama && yarn install && yarn run build && cd ../..
+cd ./lib/js_service_acala && yarn install && yarn run build && cd ../..
+
+touch .env
+echo "MNENOMIC=${MNENOMIC}" > .env
+echo "ETHEREUM_ADDRESS=${ETHEREUM_ADDRESS}" >> .env
+echo "CONTRACT_ADDRESS_LOCKDROP_TESTNET=${CONTRACT_ADDRESS_LOCKDROP_TESTNET}" >> .env
+echo "CONTRACT_ADDRESS_MXC_TESTNET=${CONTRACT_ADDRESS_MXC_TESTNET}" >> .env
+echo "CONTRACT_ADDRESS_IOTA_PEGGED_TESTNET=${CONTRACT_ADDRESS_IOTA_PEGGED_TESTNET}" >> .env
+echo "CONTRACT_ADDRESS_LOCKDROP_MAINNET=${CONTRACT_ADDRESS_LOCKDROP_MAINNET}" >> .env
+echo "CONTRACT_ADDRESS_MXC_MAINNET=${CONTRACT_ADDRESS_MXC_MAINNET}" >> .env
+echo "CONTRACT_ADDRESS_IOTA_PEGGED_MAINNET=${CONTRACT_ADDRESS_IOTA_PEGGED_MAINNET}" >> .env
+echo "INFURA_API_PROJECT_ID=${INFURA_API_PROJECT_ID}" >> .env
+echo "ENVIRONMENT=${ENVIRONMENT}" >> .env
+
 # build APK
 flutter build apk --flavor prod
 
--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -7,13 +7,29 @@ set -e
 set -x
 
 cd ..
-git clone -b beta https://github.com/flutter/flutter.git
+git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
-flutter clean
 flutter channel stable
 flutter doctor -v
-
 echo "Installed flutter to `pwd`/flutter"
+
+NODE_VERSION="12.16.0"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+cd ./lib/js_service_kusama && yarn install && yarn run build && cd ../..
+cd ./lib/js_service_acala && yarn install && yarn run build && cd ../..
+
+touch .env
+echo "MNENOMIC=${MNENOMIC}" > .env
+echo "ETHEREUM_ADDRESS=${ETHEREUM_ADDRESS}" >> .env
+echo "CONTRACT_ADDRESS_LOCKDROP_TESTNET=${CONTRACT_ADDRESS_LOCKDROP_TESTNET}" >> .env
+echo "CONTRACT_ADDRESS_MXC_TESTNET=${CONTRACT_ADDRESS_MXC_TESTNET}" >> .env
+echo "CONTRACT_ADDRESS_IOTA_PEGGED_TESTNET=${CONTRACT_ADDRESS_IOTA_PEGGED_TESTNET}" >> .env
+echo "CONTRACT_ADDRESS_LOCKDROP_MAINNET=${CONTRACT_ADDRESS_LOCKDROP_MAINNET}" >> .env
+echo "CONTRACT_ADDRESS_MXC_MAINNET=${CONTRACT_ADDRESS_MXC_MAINNET}" >> .env
+echo "CONTRACT_ADDRESS_IOTA_PEGGED_MAINNET=${CONTRACT_ADDRESS_IOTA_PEGGED_MAINNET}" >> .env
+echo "INFURA_API_PROJECT_ID=${INFURA_API_PROJECT_ID}" >> .env
+echo "ENVIRONMENT=${ENVIRONMENT}" >> .env
 
 flutter build ios --release --no-codesign
